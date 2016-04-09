2db992fd5ac229a70037e943b865ba2fd2c719bc
Afaan Naqvi & Chris Mark & ctkochan22 & Shawn Watson <pair+afaan5556.chdmark.ctkochan22.its-swats@devbootcamp.com>
2016-04-09 14:24:46 -0700

    Create request controller

--- app/controllers/request_controller.rb
+++ app/controllers/request_controller.rb
@@ -1,2 +1,51 @@
 class RequestController < ApplicationController
+
+  def index
+    @requests = Request.all
+  end
+
+  def new
+
+  end
+
+  def show
+    @request = Request.find(params[:id])
+  end
+
+  def create
+    @request = Request.new
+  end
+
+  def edit
+    @request = Request.find(params[:id])
+  end
+
+  def update
+    @request = Request.find(params[:id])
+    @request.update(request_params)
+    if @request.save
+      redirect_to request_path(@request.id)
+    else
+      redirect_to user_path(current_user.id)
+    end
+  end
+
+  def destroy
+    @request = Request.find(params[:id])
+    @request.destroy
+    redirect_to user_path(current_user.id)
+  end
+
+  private
+
+  def request_params
+    params.require(:request).permit(
+      :match?,
+      :driver?,
+      :origin_loc,
+      :destination_loc
+      :user_id
+      )
+  end
+
 end
