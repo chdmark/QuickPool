b9075f1b42deeef3b09c9ec1a5c518f53c5ee80d
Afaan Naqvi & Chris Mark & ctkochan22 & Shawn Watson <pair+afaan5556.chdmark.ctkochan22.its-swats@devbootcamp.com>
2016-04-09 11:53:20 -0700

    Update user assocations to allow for ridden_trips and driven_trips

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   has_many :requests
-  has_many :trips, foreign_key: :driver_id
-  has_many :trips, foreign_key: :rider_id
+  has_many :driven_trips, foreign_key: :driver_id, class_name: 'Trip'
+  has_many :ridden_trips, foreign_key: :rider_id, class_name: 'Trip'
 end
