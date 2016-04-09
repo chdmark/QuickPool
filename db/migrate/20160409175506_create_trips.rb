43d29fc866d44de852ab252ddfb006e2689b3a84
Afaan Naqvi & Chris Mark & ctkochan22 & Shawn Watson <pair+afaan5556.chdmark.ctkochan22.its-swats@devbootcamp.com>
2016-04-09 11:21:20 -0700

    Creates requests table and migration

--- db/migrate/20160409175506_create_trips.rb
+++ db/migrate/20160409175506_create_trips.rb
@@ -1,8 +1,8 @@
 class CreateTrips < ActiveRecord::Migration
   def change
     create_table :trips do |t|
-      t.integer :rider_id
-      t.integer :driver_id
+      t.integer :rider_id, null: false
+      t.integer :driver_id, null: false
       t.float :origin_loc, array: true, default: []
       t.float :destination_loc, array: true, default: []
 
