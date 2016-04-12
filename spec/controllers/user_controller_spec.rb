1972cb517d9da8df6cbc1ede440ad1af0a3b1991
Chris Mark <chdmark@gmail.com>
2016-04-11 22:59:10 -0700

    Test for user show page

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe UserController, type: :controller do
+	let (:other_user){FactoryGirl.create(:user)}
 	describe '#new' do 
 		it 'should load new form for user sign up' do 
 			get :new 
@@ -13,7 +14,21 @@ RSpec.describe UserController, type: :controller do
 		it 'creates a new user and changes count by 1' do 
 			expect{ post :create, user: test_user.attributes}.to change{User.count}.by(1)
 		end
-	
+
+		# it 'will not create a new user if params are missing' do 
+
+
+			
+		# end
 	end
+
+	describe '#show' do 
+		it 'renders the user display page' do 
+			session[:user_id] = other_user.id
+			get :show, id: other_user.id
+			expect(response).to render_template(:show)
+		end
+	end
+	
 
 end
