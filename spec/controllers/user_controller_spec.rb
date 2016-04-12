cf1b62f50830649573b0a5ec3ab6c61cfefb7515
Chris Mark <chdmark@gmail.com>
2016-04-12 11:19:16 -0700

    Add controller test for edit path

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe UserController, type: :controller do
+	let (:test_user){FactoryGirl.create(:user)}
 	let (:other_user){FactoryGirl.create(:user)}
 	describe '#new' do 
 		it 'should load new form for user sign up' do 
@@ -10,7 +11,6 @@ RSpec.describe UserController, type: :controller do
 	end
 
 	describe '#create' do 
-		let (:test_user){FactoryGirl.create(:user)}
 		it 'creates a new user and changes count by 1' do 
 			expect{ post :create, user: test_user.attributes}.to change{User.count}.by(1)
 		end
@@ -28,6 +28,21 @@ RSpec.describe UserController, type: :controller do
 			get :show, id: other_user.id
 			expect(response).to render_template(:show)
 		end
+	end
+
+	describe "#edit" do 
+		it 'renders the edit page if the user is logged in' do
+			session[:user_id] = other_user.id
+			get :edit, id: other_user.id
+			expect(response).to render_template(:edit)
+		end
+
+		it 'redirects to root path if the user is not the current user' do 
+			get :edit, id: other_user.id
+			expect(response).to redirect_to root_path
+
+		end
+
 	end
 	
 
